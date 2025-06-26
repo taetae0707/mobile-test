d48100c693b07601476717a6b42ca03c0a94d0ab
taetae0707 <>
2025-06-26 12:48:55 +0000

    refactor: build 출력 디렉토리 변경하기

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig = {
-	distDir: "./dist", // 빌드 출력 디렉터리를 `./dist/`로 변경합니다.
-	// 기타 next.js 설정
+  distDir: "./.next", // 빌드 출력 디렉터리를 `./dist/`로 변경합니다.
+  // 기타 next.js 설정
 };
 
 export default withBundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 })(nextConfig);
