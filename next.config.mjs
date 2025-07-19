6ed90d8e08aafc100b7169e65d038e70a6eb871c
taetae0707 <>
2025-07-19 10:39:02 +0000

    refactor: 운영서버 소스코드 난독화 적용하여 배포

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,35 @@
 /** @type {import('next').NextConfig} */
 
 import withBundleAnalyzer from "@next/bundle-analyzer";
+import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig = {
-	distDir: "./.next", // 빌드 출력 디렉터리를 `./dist/`로 변경합니다.
-	swcMinify: true, //코드 압축 및 최적화를 보장
-	compiler: {
-		modularizeImports: {
-			"@mui/material": {
-				transform: "@mui/material/{{member}}",
-			},
-			"@mui/icons-material": {
-				transform: "@mui/icons-material/{{member}}",
-			},
-		},
-	},
+  distDir: "./.next",
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+
+  // webpack을 compiler 밖으로 이동
+  webpack: (config) => {
+    config.optimization.minimizer.push(
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // console.log 제거
+            passes: 3, // 압축 최적화를 3번 반복 (압축률 증가)
+          },
+          mangle: true, // 변수 및 함수명을 난독화하여 크기 감소
+          format: {
+            comments: false, // 모든 주석 제거 (파일 크기 줄이기)
+          },
+        },
+        extractComments: false, // 주석을 별도 파일로 분리 X (파일 크기 줄이기)
+        parallel: true, // 병렬 실행 활성화 (빌드 속도 최적화)
+      }),
+    );
+    return config;
+  },
 };
 
 export default withBundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 })(nextConfig);
