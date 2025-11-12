0409e262f2b403844cbbc4ad74e0c0e2adf690fe
taetae0707 <>
2025-11-12 14:19:35 +0000

    fix

--- src/components/jobsPage/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { Badge } from "@jobsPage/Common/Badge";
-export { CompanyLogo } from "@jobsPage/Common/CompanyLogo";
-export { SkillIcon } from "@jobsPage/Common/SkillIcon";
-export { LoadingState } from "@jobsPage/Common/LoadingState";
